c607808048e9e63a6a6b066e597714f7f6d71879
Alexey Mazurik <alexeymazurik@gmail.com>
2014-11-07 17:28:48 +0200

    added full functionality

--- js/script.js
+++ js/script.js
@@ -15,8 +15,8 @@ var Region = function Region(options) {
     this.instanceOf = function() {
         return _instanceOf;
     };
-    console.log(this.name);
-    console.log(this.children);
+    //console.log(this.name);
+    //console.log(this.children);
 };
 
 Region.prototype.addChildren = function(kids){
@@ -28,14 +28,36 @@ Region.prototype.addChildren = function(kids){
 };
 
 Region.prototype.setState = function setState(parentStatus) {
-    var status = arguments.length > 1 ?  !this.checked() : parentStatus;
-    this.children.forEach(function(child) {
-        if(child.instanceOf() == 'Region') {
-            child.setState(status);
-        }
-    });
 
-    this.checked(arguments.length > 1 ? status : parentStatus);
+
+    console.log(parentStatus);
+    parentStatus.checked(!parentStatus.checked())
+    function toChilds(status, node) {
+        if (node.children) {
+            node.children.forEach(function(child){
+                if (child.instanceOf() == 'Region') {
+                    child.checked(status);
+                }
+                toChilds(status, child);
+            });
+        }
+        else return;
+    }
+
+    function toParents() {
+
+    }
+    //debugger;
+    toChilds(parentStatus.checked(), parentStatus);
+
+    //var status = arguments.length > 1 ?  !this.checked() : parentStatus;
+    //this.children.forEach(function(child) {
+    //    if(child.instanceOf() == 'Region') {
+    //        child.setState(status);
+    //    }
+    //});
+    //
+    //this.checked(arguments.length > 1 ? status : parentStatus);
 };
 
 Region.prototype.addChild = function addChild(child, parentNode) {
@@ -71,23 +93,23 @@ Region.prototype.addChild = function addChild(child, parentNode) {
 //    return parent;
 //};
 
-Region.checkIfChildrenChecked = function(node) {
-    var result = true;
-
-    var recursion = function(item) {
-        item.children.forEach(function(child) {
-            if(!child.checked()) {
-                result = false;
-                return;
-            } else {
-                recursion(child);
-            }
-        });
-    };
-
-    recursion(node);
-
-};
+//Region.checkIfChildrenChecked = function(node) {
+//    var result = true;
+//
+//    var recursion = function(item) {
+//        item.children.forEach(function(child) {
+//            if(!child.checked()) {
+//                result = false;
+//                return;
+//            } else {
+//                recursion(child);
+//            }
+//        });
+//    };
+//
+//    recursion(node);
+//
+//};
 
 var viewModel = function() {
 
@@ -96,8 +118,45 @@ var viewModel = function() {
     self.companyName    = ko.observable('');
     self.companyURL     = ko.observable('');
     self.adultContent   = ko.observable('false');
+    self.sex            = ko.observable('any');
+    self.sumBudget      = ko.observable('');
+    self.perDayBudget   = ko.observable('');
+    self.adsSubject     = ko.observable('');
+
+    self.remained       = ko.computed(function(){
+        return (30 - self.companyName().length).toString();
+    });
 
 
+
+    self.returnSendForm = function() {
+
+        var tmp2 = [];
+
+        var categories = self.categories.categories;
+
+        for (var i = 0; i < categories.length; i++) {
+            if (categories[i].checked())
+                tmp2.push(categories[i].name);
+        }
+
+        var sendForm = {
+            name: self.companyName(),
+            url: self.companyURL(),
+            adultContent: self.adultContent(),
+            sex: self.sex(),
+            sumBudget: self.sumBudget(),
+            perDayBudget: self.perDayBudget(),
+            subject: self.adsSubject(),
+            categories: tmp2
+        };
+
+
+        alert('Отправлено: ' + ko.toJSON(sendForm));
+
+
+        return ko.toJSON(sendForm);
+    };
 
     self.geo = new Region({
         name: 'Все страны',
@@ -156,19 +215,87 @@ var viewModel = function() {
         ]
     });
 
-    //console.log(self.geo.children.length);
 
-    //self.geo.setChecked();
+    self.categories = {
+        checked: ko.observable(false),
+        name: 'Категория',
+        minPrice: 'Минимальн. Цена',
+        recommendedPrice: 'Рекомендов. Цена',
+        stablePrice: 'Установлен. Цена',
+        categories: [
+            {
+                checked: ko.observable(false),
+                name: 'Авто/Мото',
+                minPrice: '0.50',
+                recommendedPrice: '0.50',
+                stablePrice:'0.50'
+            },
+            {
+                checked: ko.observable(false),
+                name: 'Бизнес и финансы',
+                minPrice: '1.50',
+                recommendedPrice: '1.50',
+                stablePrice:'1.50'
+            },
+            {
+                checked: ko.observable(false),
+                name: 'Дом и быт',
+                minPrice: '0.50',
+                recommendedPrice: '0.50',
+                stablePrice:'0.50'
+            },
+            {
+                checked: ko.observable(false),
+                name: 'Красота и здоровье',
+                minPrice: '1.89',
+                recommendedPrice: '1.89',
+                stablePrice:'1.89'
+            },
+            {
+                checked: ko.observable(false),
+                name: 'Культура и искусство',
+                minPrice: '0.50',
+                recommendedPrice: '0.50',
+                stablePrice:'0.50'
+            },
+            {
+                checked: ko.observable(false),
+                name: 'Мода и стиль',
+                minPrice: '1.50',
+                recommendedPrice: '1.50',
+                stablePrice:'1.50'
+            },
+            {
+                checked: ko.observable(false),
+                name: 'Недвижимость',
+                minPrice: '0.50',
+                recommendedPrice: '0.50',
+                stablePrice:'0.50'
+            }
+        ]
+    };
 
-    //Region.prototype.toJSON = function() {
-    //    var result = {
-    //        name    : this.name,
-    //        checked : this.checked(),
-    //        children: (function() {
-    //
-    //        })()
-    //    };
-    //};
+    self.changeChildren = function(){
+
+        self.categories.categories.forEach(function(child){
+            child.checked(self.categories.checked());
+        });
+
+        return true;
+    };
+
+    self.changeParent = function() {
+        var flag = true;
+
+        self.categories.categories.forEach(function(child){
+            if (!child.checked()) {
+                flag = false;
+            }
+        });
+
+        self.categories.checked(flag);
+        return true;
+    };
 };
 
 ko.applyBindings(new viewModel());
